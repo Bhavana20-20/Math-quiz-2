3d18d23ca60b8b58f3f660956e54a62e20ffeaeb
Bhavana20-20 <76871796+Bhavana20-20@users.noreply.github.com>
2021-05-13 11:45:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var player1_name = localStorage.getItem("player_1")
+var player2_name = localStorage.getItem("player_2")
+var player1score = 0;
+var player2score = 0;
+document.getElementById("player1_name").innerHTML=player1_name;
+document.getElementById("player2_name").innerHTML=player2_name;
+document.getElementById("player1_score").innerHTML=":"+player1score;
+document.getElementById("player2_score").innerHTML=":"+player2score;
+document.getElementById("player_question").innerHTML="Question Turn:"+player1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn:"+player2_name;
